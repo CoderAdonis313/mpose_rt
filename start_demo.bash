a8fb560d08b3a91b037e72ec95a58230805af441
CoderAdonis313 <coderadonis@gmail.com>
2026-09-24 07:27:54 -0400

    saving more data

--- start_demo.bash
+++ start_demo.bash
@@ -5,6 +5,6 @@ echo "INFO: Running pose tracking on Video"
 python rt_live.py \
   --project-root /home/abhi/dev/mpose_rt \
   --cam_source 0 \
-  --mesh BaselinePointer.obj \
+  --mesh PointerActual.obj \
   --cam_file zed_1080p_raw_calib.json \
   --udp-port 5005
